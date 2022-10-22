3056d2f0a47d3e229efba7630ef989cf29843a5f
LikHang <likhang@upstackstudio.com>
2022-10-22 14:23:52 +0800

    initial

---
+++ src/screens/homeScreen.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import {SafeViewArea, View, Text} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeViewArea>
+      <Text>HomeScreen</Text>
+    </SafeViewArea>
+  );
+};
+
+export default HomeScreen;
